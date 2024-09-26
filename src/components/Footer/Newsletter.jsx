a1ed1c2bc98023c721d9062a13920405a40f71f4
Getaye Asfaw <asfawgetaye123@gmail.com>
2024-09-25 22:35:05 -0700

    get push css handled

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import SubHeading from "../SubHeading/SubHeading";
 import "./Newsletter.css";
 
@@ -7,14 +5,13 @@ const Newsletter = () => (
   <div className="app__newsletter">
     <div className="app__newsletter-heading">
       <SubHeading title="Newsletter" />
-      <h1 className="headtext__cormorant">Subscribe to our newsletter</h1>
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
       <p className="p__opensans">And never miss latest Updates!</p>
     </div>
     <div className="app__newsletter-input flex__center">
       <input type="email" placeholder="Enter your email address" />
-      <button className="cutom___button">Subscribe</button>
+      <button type="button" className="custom__button">Subscribe</button>
     </div>
-    Newsletter
   </div>
 );
 
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -3,16 +3,17 @@ import { images, data } from "../../constants";
 
 import "./Laurels.css";
 
-const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+const Awardcard = ({ award: { imgUrl, title, subtitle } }) => (
   <div className="app__laurels_awards-card">
-    <img src={imgUrl} alt="awards" />
+    <img src={imgUrl} alt="award" className="app__laurels_img" />
     <div className="app__laurels_awards-card_content">
-      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
-      <p className="p__opensans">{subtitle}</p>
+      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+        {title}
+      </p>
+      <p className="p__cormorant">{subtitle}</p>
     </div>
   </div>
 );
-
 const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
     <div className="app__wrapper_info">
@@ -20,14 +21,16 @@ const Laurels = () => (
       <h1 className="headtext__cormorant">Our Laurels</h1>
 
       <div className="app__laurels_awards">
-        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+        {data.awards.map((award) => (
+          <Awardcard award={award} key={award.title} />
+        ))}
       </div>
     </div>
-
     <div className="app__wrapper_img">
-      <img src={images.kitfo} alt="laurels_img" />
+      <img src={images.kitfo} alt="laurels" />
     </div>
   </div>
 );
+
 
 export default Laurels;
