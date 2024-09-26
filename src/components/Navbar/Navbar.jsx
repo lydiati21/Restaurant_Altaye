24ed2c809960db2349dab390eb8acc7dec5358ec
Getaye Asfaw <asfawgetaye123@gmail.com>
2024-09-25 22:19:34 -0700

    first push from get

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,121 @@
+import   { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+
+import "./Navbar.css";
+
+import "./Navbar.css";
+
+// const Navbar = () => {
+//   const [togglemenu, setToggleMenu] = useState(false);
+
+//   return (
+//     <nav className="app__navbar">
+//       <div className="app__navbar-logo ">
+//         <img src={images.altaye1} alt="app logo" />
+//         {/* <img src="app.logomakr.com/4bxAO9" alt="app logo" /> */}
+//       </div>
+//       <ul className="app__navbar-links">
+//         <li className="p__opensans">
+//           <a href="#home">Home</a>
+//         </li>
+//         <li className="p__opensans">
+//           <a href="#about">About</a>
+//         </li>
+//         <li className="p__opensans">
+//           <a href="#menu">Menu</a>
+//         </li>
+//         <li className="p__opensans">
+//           <a href="#awards">Awards</a>
+//         </li>
+//         <li className="p__opensans">
+//           <a href="#contact">Contact</a>
+//         </li>
+//       </ul>
+//       <div className="app__navbar-login">
+//         <a href="#login" className="p__opensans">
+//           {/* Log In / Register */}
+//         </a>
+//         <div />
+//         <a href="/" className="p__opensans">
+//           Book Table
+//         </a>
+//       </div>
+//       <div className="app___navbar -smallscreen">
+//         <GiHamburgerMenu
+//           color="#fff"
+//           fontSize={27}
+//           onClick={() => setToggleMenu(true)}
+//         />
+
+//         {togglemenu && (
+//           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom ">
+//             <MdOutlineRestaurantMenu
+//               fontSize={27}
+//               className="overlay__close"
+//               onClick={() => setToggleMenu(false)}
+//             />
+//             <ul className="app__navbar-smallscreen_links">
+//               <li className="p__opensans">
+//                 <a href="#home">Home</a>
+//               </li>
+//               <li className="p__opensans">
+//                 <a href="#about">About</a>
+//               </li>
+//               <li className="p__opensans">
+//                 <a href="#menu">Menu</a>
+//               </li>
+//               <li className="p__opensans">
+//                 <a href="#awards">Awards</a>
+//               </li>
+//               <li className="p__opensans">
+//                 <a href="#contact">Contact</a>
+//               </li>
+//             </ul>
+//           </div>
+//         )}
+//       </div>
+//     </nav>
+//   );
+// };
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.altaye1} alt="app__logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
+      {/* <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">Log In / Registration</a>
+        <div />
+        <a href="/" className="p__opensans">Book Table</a>
+      </div> */}
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <ul className="app__navbar-smallscreen_links">
+              <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/images.js
@@ -0,0 +1,69 @@
+import bg from "../assets/bg.png";
+import chef from "../assets/chef.png";
+import G from "../assets/G.png";
+import gallery01 from "../assets/gallery01.png";
+import gallery02 from "../assets/gallery02.png";
+import gallery03 from "../assets/gallery03.png";
+import gallery04 from "../assets/gallery04.png";
+import knife from "../assets/knife.png";
+import menu from "../assets/menu.png";
+import tej from "../assets/tej.png";
+import combo from "../assets/combo.png";
+import tibs from "../assets/tibs.png";
+import kitfo from "../assets/kitfo.png";
+import vegicombo from "../assets/vegicombo.png";
+import coffee from "../assets/coffee.png";
+import kurt from "../assets/kurt.png";
+import doro from "../assets/doro.png";
+import shiro from "../assets/shiro.png";
+import genfo from "../assets/genfo.png";
+import mesob from "../assets/mesob.png";
+import overlaybg from "../assets/overlaybg.png";
+import spoon from "../assets/spoon.svg";
+import welcome from "../assets/welcome.png";
+import findus from "../assets/findus.png";
+import laurels from "../assets/laurels.png";
+import award01 from "../assets/award01.png";
+import award02 from "../assets/award02.png";
+import award03 from "../assets/award03.png";
+import award05 from "../assets/award05.png";
+import sign from "../assets/sign.png";
+import quote from "../assets/quote.png";
+import altaye1 from "../assets/altaye1.png";
+import fooda from "../assets/fooda1.png"
+
+export default {
+  bg,
+  chef,
+  G,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  knife,
+  menu,
+  tej,
+  kitfo,
+  vegicombo,
+  coffee,
+  kurt,
+  doro,
+  shiro,
+  genfo,
+  combo,
+  tibs,
+  mesob,
+  overlaybg,
+  spoon,
+  welcome,
+  findus,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
+  sign,
+  quote,
+  altaye1,
+  fooda,
+};
